4957b66faa36e424e1d8f28d44b5d797f6f759b4
msig0000 <msig301331>
2013-07-09 14:58:59 +0600

    My Programm HA Ha

---
+++ Bakul/src/Bakul_Class.java
@@ -0,0 +1,13 @@
+
+public class Bakul_Class {
+
+	public Bakul_Class() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Im Bakul");
+
+	}
+
+}
